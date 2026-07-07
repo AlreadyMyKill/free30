874c33eb1a5c90587741596bb6c36823e77c5ef3
Jan Fiedler <fiedler06@seznam.cz>
2026-07-07 17:49:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cista-hlava-v1";
+const CACHE = "cista-hlava-v2";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
